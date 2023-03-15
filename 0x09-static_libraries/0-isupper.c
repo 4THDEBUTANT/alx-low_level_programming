7710cdf8f2b0dffc25ddf1aee7b9919ccd8b44a6
David Kamau <davidkamau@zetech.ac.ke>
2023-03-15 15:17:53 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		0-strcat.c
+			1-isdigit.c
+			1-memcpy.c
+			1-strncat.c 2-strncpy.c 3-islower.c 4-strpbrk.c 6-abs.c
+			100-atoi.c
+			2-strchr.c 2-strlen.c
+			3-puts.c
+			3-strcmp.c 3-strspn.c
+			4-isalpha.c
+			5-strstr.c
+			9-strcpy.c
